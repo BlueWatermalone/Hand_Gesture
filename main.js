093fb750e9ca67116f599324ce7c6fe1dbc56b47
BlueWatermalone <71526538+BlueWatermalone@users.noreply.github.com>
2021-03-23 17:45:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function takeSnapshot () {
 
 console.log('ml5 version: ', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qz_P-zSc-/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/k08AcyAu0/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded!")
